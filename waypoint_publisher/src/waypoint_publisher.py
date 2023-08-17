1e97c65a1d4096361b3b09d73d5b76834a148177
AbBaSaMo <s.a.baaset.moslih@gmail.com>
2023-08-17 15:23:57 +1000

    Updated references to package name after name change

--- waypoint_publisher/src/waypoint_publisher.py
+++ waypoint_publisher/src/waypoint_publisher.py
@@ -1,24 +1,28 @@
 import rclpy
 from rclpy.node import Node
 
-from std_msgs.msg import String
+from waypoint_publisher.msg import Waypoint
 
 
-class MinimalPublisher(Node):
+class WaypointPublisher(Node):
 
     def __init__(self):
-        super().__init__('minimal_publisher')
-        self.publisher_ = self.create_publisher(String, 'topic', 10)
+        super().__init__('waypoint_publisher')
+        self.publisher_ = self.create_publisher(Waypoint, '/goal_waypoint', 10)
+        
         timer_period = 0.5  # seconds
-        self.timer = self.create_timer(timer_period, self.timer_callback)
-        self.i = 0
+        self.timer = self.create_timer(timer_period, self.waypoint_callback)
 
-    def timer_callback(self):
-        msg = String()
-        msg.data = 'Hello World: %d' % self.i
+    def waypoint_callback(self):
+        """ Publishes a fixed global waypoint consisting of an x, y, z co-ordinate and radius
+        
+        
+        """
+        msg = Waypoint()
+        print(Waypoint)
+        
         self.publisher_.publish(msg)
         self.get_logger().info('Publishing: "%s"' % msg.data)
-        self.i += 1
 
 
 def main(args=None):
