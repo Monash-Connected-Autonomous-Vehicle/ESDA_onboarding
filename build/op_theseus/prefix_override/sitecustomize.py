8f72a78930bde8456668d5681baff8fb199e3793
AbBaSaMo <95030427+AbBaSaMo@users.noreply.github.com>
2023-08-20 20:35:46 +1000

    Complete OP Task 1
    Task completed on Thursday, pushed into our branch today

---
+++ build/op_theseus/prefix_override/sitecustomize.py
@@ -0,0 +1,3 @@
+import sys
+sys.real_prefix = sys.prefix
+sys.prefix = sys.exec_prefix = '/home/mcav/dbat/ESDA_onboarding/install/op_theseus'
---
+++ build/waypoint_pub/colcon_command_prefix_setup_py.sh
@@ -0,0 +1,2 @@
+# generated from colcon_core/shell/template/command_prefix.sh.em
+. "/home/mcav/dbat/ESDA_onboarding/install/custom_interfaces/share/custom_interfaces/package.sh"
